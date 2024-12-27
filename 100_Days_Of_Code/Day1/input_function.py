15afb55fcf722fc26b8ecefbea3400be7a47b96a
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2024-12-27 22:07:10 +0530

    Create input_function.py

---
+++ 100_Days_Of_Code/Day1/input_function.py
@@ -0,0 +1,5 @@
+# Taking input from the user and performing calculations
+number1 = float(input("Enter the first number: "))  # Convert input to float
+number2 = float(input("Enter the second number: "))
+sum_result = number1 + number2
+print(f"The sum of {number1} and {number2} is {sum_result}")
